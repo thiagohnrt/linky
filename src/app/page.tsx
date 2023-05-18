1b4309f634d3de63ae7b67a2be2f9dbbd6a4adf6
Thiago Honorato <thiagohbhonorato@gmail.com>
2023-05-18 13:39:08 -0300

    New header layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,35 @@
 import Icon from "@/components/header/Icon";
 import ThemeIcon from "@/components/header/ThemeIcon";
-import { FaGithub } from "react-icons/fa";
+import { Settings2Icon } from "lucide-react";
+import { FaGithub, FaBookmark } from "react-icons/fa";
 import { MdKeyboardCommandKey, MdOutlineSearch } from "react-icons/md";
 
 export default function Home() {
   return (
     <>
-      <header className="h-[55px] px-8 grid gap-2 grid-cols-3 items-center bg-neutral-200 dark:bg-neutral-950 transition-colors duration-300">
-        <span className="text-xl pon select-none">Bookmark Manager</span>
-        <div className="w-full p-2 gap-2 flex items-center cursor-pointer bg-white dark:bg-neutral-900 dark:hover:bg-neutral-800 transition-colors duration-300">
-          <MdOutlineSearch size={20} />
-          <span className="flex-auto text-neutral-400">Search</span>
-          <span className="flex items-center p-1 text-xs bg-neutral-200 dark:bg-neutral-950 transition-colors duration-300">
-            <MdKeyboardCommandKey />K
-          </span>
+      <header className="h-[55px] px-8 flex justify-between items-center bg-neutral-200 dark:bg-neutral-950 transition-colors duration-300">
+        <div className="flex items-center">
+          <FaBookmark size={20} />
+          <span className="pon select-none ml-2 text-xl">Bookmark Manager</span>
         </div>
-        <div className="flex justify-end gap-2 items-center">
-          <ThemeIcon />
-          <Icon>
-            <FaGithub size={20} />
-          </Icon>
+        <div className="flex gap-6">
+          <div className="w-64 p-2 gap-2 flex items-center cursor-pointer bg-white dark:bg-neutral-900 dark:hover:bg-neutral-800 transition-colors duration-300">
+            <MdOutlineSearch size={20} />
+            <span className="flex-auto text-neutral-400">Search</span>
+            <span className="flex items-center p-1 text-xs bg-neutral-200 dark:bg-neutral-950 transition-colors duration-300">
+              <MdKeyboardCommandKey />K
+            </span>
+          </div>
+          <div className="flex justify-end gap-2 items-center">
+            <Icon>
+              <Settings2Icon size={20} />
+            </Icon>
+            <ThemeIcon />
+            <div className="w-px h-6 bg-black dark:bg-white"></div>
+            <Icon>
+              <FaGithub size={20} />
+            </Icon>
+          </div>
         </div>
       </header>
       <main className="h-[calc(100vh-55px)] flex">
