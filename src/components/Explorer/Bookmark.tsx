1f67b5d81f7235b4b8274b036be91838fd003026
Thiago Honorato <thiagohbhonorato@gmail.com>
2023-07-05 11:28:23 -0300

    Layout bug fix

--- src/components/Explorer/Bookmark.tsx
+++ src/components/Explorer/Bookmark.tsx
@@ -12,7 +12,11 @@ interface BookmarkProps {
 export function Bookmark({ url, name }: BookmarkProps) {
   return (
     <div className="flex justify-between pl-4 py-1 [&>.bookmark-more]:hover:opacity-100 hover:bg-neutral-200 dark:hover:bg-neutral-900 transition-colors duration-300">
-      <Link href={url} target="_blank" className="flex gap-2 w-full">
+      <Link
+        href={url}
+        target="_blank"
+        className="flex gap-2 w-full items-center"
+      >
         <Favicon url={url} />
         <span>{name}</span>
       </Link>
