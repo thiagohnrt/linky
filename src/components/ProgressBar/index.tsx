73dba4344e06c79bbf73d907d9e2415a2df003d0
Thiago Honorato <thiagohbhonorato@gmail.com>
2024-02-28 11:12:35 -0300

    Add attribute and change progress bar color

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,20 +1,23 @@
 import * as RP from "@radix-ui/react-progress";
+import { CSSProperties } from "react";
 
 interface ProgressBarProps {
   value: number;
   className?: string;
+  style?: CSSProperties;
 }
 
-export function ProgressBar({ value, className }: ProgressBarProps) {
+export function ProgressBar({ value, className, style }: ProgressBarProps) {
   return (
     <RP.Root
       className={["relative overflow-hidden translate-y-0", className].join(
         " "
       )}
       value={value}
+      style={style}
     >
       <RP.Indicator
-        className="bg-blue-900 w-full h-full"
+        className="bg-purple-950 w-full h-full"
         style={{
           transition: "transform 100ms cubic-bezier(0.65, 0, 0.35, 1)",
           transform: `translateX(-${100 - value}%)`,
