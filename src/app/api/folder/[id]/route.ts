d5db6e3b086d6c8cce10aa188d370e970da94a59
Thiago Honorato <thiagohbhonorato@gmail.com>
2024-02-29 14:15:06 -0300

    Delete bookmarks with folder

--- src/app/api/folder/[id]/route.ts
+++ src/app/api/folder/[id]/route.ts
@@ -1,4 +1,5 @@
 import { Folder } from "@/interfaces/Folder";
+import BookmarkModel from "@/models/Bookmark";
 import FolderModel from "@/models/Folder";
 import dbConnection from "@/services/mongodb";
 import { NextRequest, NextResponse } from "next/server";
@@ -23,5 +24,6 @@ export async function DELETE(
 ) {
   await dbConnection();
   await FolderModel.deleteOne({ _id: params.id });
+  await BookmarkModel.deleteMany({ folderId: params.id });
   return new Response(null, { status: 204 });
 }
