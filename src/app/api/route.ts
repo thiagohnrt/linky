2a45a799dde33808c073dc0c1fa565879bf05ecb
Thiago Honorato <thiagohbhonorato@gmail.com>
2024-03-11 11:19:22 -0300

    Fix cached route

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,9 +3,11 @@ import { Folder } from "@/interfaces/Folder";
 import BookmarkModel from "@/models/Bookmark";
 import FolderModel from "@/models/Folder";
 import dbConnection from "@/services/mongodb";
+import { unstable_noStore as noStore } from "next/cache";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  noStore();
   await dbConnection();
   const folders = await FolderModel.find<Folder>({});
   const bookmarks = await BookmarkModel.find<Bookmark>({});
