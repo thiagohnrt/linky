e9892e35f287f48a90158bb69c78b245cf86846f
Thiago Honorato <thiagohbhonorato@gmail.com>
2024-03-18 16:57:52 -0300

    Add message

--- src/components/Settings/ImportBookmark.tsx
+++ src/components/Settings/ImportBookmark.tsx
@@ -2,6 +2,7 @@
 
 import { ChangeEvent, ReactNode, useState } from "react";
 import { Button } from "../Form/Button";
+import { UploadIcon } from "lucide-react";
 
 interface ImportBookmarkProps {
   title: string;
@@ -40,27 +41,28 @@ export function ImportBookmark({
         accept={acceptFile}
         className="w-full p-2 border-2 border-dashed border-neutral-400 cursor-pointer"
       />
-      <div className="mt-8">
-        <Button
-          color={
-            status === "success"
-              ? "success"
-              : status === "error"
-              ? "danger"
-              : "primary"
-          }
-          className="rounded-md "
-          onClick={onImport}
-          disabled={!file}
-          loading={status === "loading"}
-        >
-          {status === "success"
-            ? "Success"
+      <p className="my-8 flex items-center">
+        <UploadIcon size={18} className="mr-2" /> Import your bookmarks.
+      </p>
+      <Button
+        color={
+          status === "success"
+            ? "success"
             : status === "error"
-            ? "Error. Try again."
-            : "Import"}
-        </Button>
-      </div>
+            ? "danger"
+            : "primary"
+        }
+        className="rounded-md "
+        onClick={onImport}
+        disabled={!file}
+        loading={status === "loading"}
+      >
+        {status === "success"
+          ? "Success"
+          : status === "error"
+          ? "Error. Try again."
+          : "Import"}
+      </Button>
     </div>
   );
 }
