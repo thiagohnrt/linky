4d50b093a4d19c14b0008582b0bcdf227b924c71
Thiago Honorato <thiagohbhonorato@gmail.com>
2024-04-15 17:14:42 -0300

    Debug error

--- src/app/(app)/settings/account/page.tsx
+++ src/app/(app)/settings/account/page.tsx
@@ -3,14 +3,18 @@ import { SettingsPage } from "@/components/Settings/SettingsPage";
 import { User } from "@/interfaces/User";
 import { cookies } from "next/headers";
 
-async function getMe(): Promise<User> {
+async function getMe(): Promise<User | any> {
   const response = await fetch(`${process.env.API_URL}/api/me`, {
     cache: "no-store",
     headers: {
       Cookie: cookies().toString(),
     },
   });
-  return await response.json();
+  if (response.status === 200) {
+    return await response.json();
+  } else {
+    return response.body;
+  }
 }
 
 export default async function Account() {
@@ -21,7 +25,7 @@ export default async function Account() {
         <div>
           <p className="mb-1">You are logged in as:</p>
           <span className="border border-input px-2 rounded-sm bg-neutral-800">
-            {user.username}
+            {user}
           </span>
         </div>
         <AccountLogOut />
