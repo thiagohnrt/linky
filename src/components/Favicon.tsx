9302323eb920630a2b7285a6cd70bf56655789b8
Thiago Honorato <thiagohbhonorato@gmail.com>
2024-04-15 16:47:14 -0300

    Remove API favicon

--- src/components/Favicon.tsx
+++ src/components/Favicon.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { GlobeIcon } from "lucide-react";
 import Image from "next/image";
 import { Bookmark } from "@/interfaces/Bookmark";
+import { generateColor } from "@/services/generateColor";
 
 interface FaviconProps {
   bookmark: Bookmark;
@@ -16,13 +17,43 @@ export default function Favicon({
   size = 16,
   className = "",
 }: FaviconProps) {
+  const [hasError, setError] = useState(false);
   return (
-    <Image
-      src={`/api/favicon?url=${bookmark.url}&title=${bookmark.name}`}
-      alt="Favicon"
-      width={size}
-      height={size}
+    <>
+      {hasError ? (
+        <Avatar bookmark={bookmark} size={size} className={className} />
+      ) : (
+        <Image
+          src={"http://www.google.com/s2/favicons?domain=" + bookmark.url}
+          alt="Favicon"
+          width={size}
+          height={size}
+          className={className}
+          onLoad={() => setError(false)}
+          onError={() => setError(true)}
+        />
+      )}
+    </>
+  );
+}
+
+function Avatar({ bookmark, size = 16, className = "" }: FaviconProps) {
+  const char = bookmark.name.charAt(0);
+  const [background, color] = generateColor(char);
+  return (
+    <div
       className={className}
-    />
+      style={{
+        width: size,
+        height: size,
+        fontSize: size - 2,
+        lineHeight: `${size - 2}px`,
+        textAlign: "center",
+        background,
+        color,
+      }}
+    >
+      {char}
+    </div>
   );
 }
