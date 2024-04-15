f2b4f0db81fceb4833f75b0a459009e9f3d1cfe3
Thiago Honorato <thiagohbhonorato@gmail.com>
2024-04-15 17:21:07 -0300

    Refactoring /api/me

--- src/app/(app)/settings/account/page.tsx
+++ src/app/(app)/settings/account/page.tsx
@@ -1,21 +1,6 @@
 import { AccountLogOut } from "@/components/Settings/Account/LogOut";
 import { SettingsPage } from "@/components/Settings/SettingsPage";
-import { User } from "@/interfaces/User";
-import { cookies } from "next/headers";
-
-async function getMe(): Promise<User | any> {
-  const response = await fetch(`${process.env.API_URL}/api/me`, {
-    cache: "no-store",
-    headers: {
-      Cookie: cookies().toString(),
-    },
-  });
-  if (response.status === 200) {
-    return await response.json();
-  } else {
-    return response.body;
-  }
-}
+import { getMe } from "@/services/dataService";
 
 export default async function Account() {
   const user = await getMe();
@@ -25,7 +10,7 @@ export default async function Account() {
         <div>
           <p className="mb-1">You are logged in as:</p>
           <span className="border border-input px-2 rounded-sm bg-neutral-800">
-            {user}
+            {user.username}
           </span>
         </div>
         <AccountLogOut />
--- src/services/dataService.ts
+++ src/services/dataService.ts
@@ -1,8 +1,20 @@
 import { Bookmarks } from "@/interfaces/Bookmark";
+import { User } from "@/interfaces/User";
+import { cookies } from "next/headers";
 
 export async function getData(): Promise<Bookmarks[]> {
   const response = await fetch(`${process.env.API_URL}/api`, {
     cache: "no-store",
+  });
+  return await response.json();
+}
+
+export async function getMe(): Promise<User> {
+  const response = await fetch(`${process.env.API_URL}/api/me`, {
+    cache: "no-store",
+    headers: {
+      Cookie: cookies().toString(),
+    },
   });
   return await response.json();
 }
